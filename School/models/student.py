9f36895e04c85000d71d8901f0f8c9838f7c827f
hoangvinh121299 <42103527+hoangvinh121299@users.noreply.github.com>
2020-06-10 01:21:18 +0700

    10/6/2020

--- School/models/student.py
+++ School/models/student.py
@@ -16,21 +16,30 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value)
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class StudentStudent(models.Model):
+    # name sẽ tạo 1 bảng trong db dưới dạng student_student
     _name = 'student.student'
+    # Với các cột là các fields được định nghĩa trong class StudentStudent
+    name = fields.Char(string='Họ và tên', required=True)
+    student_dob = fields.Date(string='Ngày sinh')
+    gender = fields.Selection([('male', 'Nam'), ('female', 'Nữ')],
+                              string='Giới tính')
+    image = fields.Binary(string='Ảnh')
+    # Vùng quản lý địa chỉ
+    street = fields.Char(string='Tên đường', required=True)
+    city = fields.Many2one('res.country.state', string='Thành phố',store=True)
+    Nationality = fields.Many2one('res.country', string='Quốc gia')
 
-    name = fields.Char(string='Name', required=True)
-    age = fields.Integer(string='Age')
-    photo = fields.Binary(string='Image')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')],
-                             string='Gender')
-    student_dob = fields.Date(string="Date of Birth")
-    student_blood_group = fields.Selection(
-        [('A+', 'A+ve'), ('B+', 'B+ve'), ('O+', 'O+ve'), ('AB+', 'AB+ve'),
-        ('A-', 'A-ve'), ('B-', 'B-ve'), ('O-', 'O-ve'), ('AB-', 'AB-ve')],
-        string='Blood Group'
-    )
-    nationality = fields.Many2one('res.country', string='Nationality')
+    # grade = fields.Many2one
+    # Class = fields.Many2one
+    # link giữa lựa chọn quốc gia với các tỉnh thành tương ứng
+    @api.onchange('Nationality')
+    def set_values_to_state(self):
+        if self.Nationality:
+            ids = self.env['res.country.state'].search([('country_id', '=', self.Nationality.id)])
+            return {
+                'domain' : {'city' : [('id', 'in', ids.ids)], }
+            }
