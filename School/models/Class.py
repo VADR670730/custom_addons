53d5d83197ec1a942f886007563cdece182ad986
hoangvinh121299 <42103527+hoangvinh121299@users.noreply.github.com>
2020-06-26 22:14:03 +0700

    26/06/2020
    Update some view of Class, Grade, improve database of teacher, improve view of teacher.

--- School/models/Class.py
+++ School/models/Class.py
@@ -9,11 +9,11 @@ class ClassClass(models.Model):
     grade_id = fields.Selection([('1', 'Khối 1'), ('2', 'Khối 2'), ('3', 'Khối 3'), ('4', 'Khối 4'),
                                  ('5', 'Khối 5')], string='Khối lớp')
     # Hậu tố các lớp
-    subname = fields.Selection([('A1', 'Lớp A1'), ('A2', 'Lớp A2'), ('A3', 'Lớp A3'), ('A4', 'Lớp A4')
-                                   , ('A5', 'Lớp A5')], string='Lớp')
+    subname = fields.Selection([('A1', 'Lớp A1'),('A2', 'Lớp A2'),('A3', 'Lớp A3'), ('A4', 'Lớp A4')
+                                   ,('A5', 'Lớp A5')], string='Lớp')
     #Niên khoá
-    date_from = fields.Datetime(string='Bắt đầu')
-    date_to = fields.Datetime(string='Kết thúc')
+    date_from = fields.Char(string="Bắt đầu")
+    date_to = fields.Char(string='Kết thúc ')
     # Giáo viên chủ nhiệm
     teacher_on_duty = fields.Many2one('teacher.teacher', string='Giáo viên chủ nhiệm')
     #Sĩ số
--- School/models/teacher.py
+++ School/models/teacher.py
@@ -5,20 +5,33 @@ class TeacherTeacher(models.Model):
     # name sẽ tạo ra 1 bảng trong db có dạng teacher_teacher
     _name = 'teacher.teacher'
     # Với các cột là các fields được đinh nghĩa
+    # Vùng quản lý thông tin cơ bản giáo viên
     name = fields.Char(string='Họ và tên', required=False)
     teacher_dob = fields.Date(string='Ngày sinh')
     gender = fields.Selection([('male', 'Nam'), ('female', 'Nữ')],
                               string='Giới tính')
-    avatar = fields.Binary(string="Ảnh đại diện")
-    # Vùng quản lý địa chỉ
+    avatar = fields.Binary(string='Ảnh đại diện')
+    teacher_place_of_birth = fields.Char(string='Nơi sinh')
+    teacher_nation = fields.Char(string='Dân tộc')
+    # Vùng quản lý thông tin liên hệ
     address = fields.Char(string='Địa chỉ')
+    teacher_phone = fields.Char(string='Số điện thoại')
+    # Vùng quản lý thông tin chuyên môn
+    teacher_level = fields.Char(string='Trình độ học vấn')
+    teacher_graduate_year = fields.Char(string='Năm tốt nghiệp')
+    teacher_certificate_language = fields.Char(string='Chứng chỉ ngoại ngữ')
+    teacher_certificate_IT = fields.Char(string='Chứng chỉ tin học')
+    teacher_years_of_teaching = fields.Char(string='Số năm tham gia giảng dạy')
+    teacher_experience = fields.Char(string='Kinh nghiệm thực tế')
+    teacher_title = fields.Char(string='Danh hiệu đạt được')
+    teacher_reward = fields.Char(string='Giải thưởng đạt được')
     # Grade = fields.Many2one
     # Class = fields.Many2one
-    # link giữa lựa chọn quốc gia với các tỉnh thành tương ứng
-    @api.onchange('Nationality')
-    def set_values_to_state(self):
-        if self.Nationality:
-            ids = self.env['res.country.state'].search([('country_id', '=', self.Nationality.id)])
-            return {
-                'domain': {'city': [('id', 'in', ids.ids)], }
-            }
+    # # link giữa lựa chọn quốc gia với các tỉnh thành tương ứng
+    # @api.onchange('Nationality')
+    # def set_values_to_state(self):
+    #     if self.Nationality:
+    #         ids = self.env['res.country.state'].search([('country_id', '=', self.Nationality.id)])
+    #         return {
+    #             'domain': {'city': [('id', 'in', ids.ids)], }
+    #         }
