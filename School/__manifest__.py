05c9e8072652766754287ebff91c5f7d989bac5e
hoangvinh121299 <42103527+hoangvinh121299@users.noreply.github.com>
2020-07-21 23:01:25 +0700

    Update __manifest__.py

--- School/__manifest__.py
+++ School/__manifest__.py
@@ -3,11 +3,12 @@
     'name': "School",
 
     'summary': """
-        School""",
+        Quản lý thông tin học sinh, quản lý thông tin giáo viên, quản lý thông tin lớp học
+        """,
 
     'description': """
-        Module quản lý trường học
-    """,
+        Chức năng chính là quản lí thông tin giáo viên, học sinh, lớp học, môn học và bảng điểm. Thể hiện được mô hình quản lí học sinh tiểu học dựa trên đơn vị lớp học. Quản lí các môn học theo từng lớp, và theo từng học kỳ. Cuối mỗi học kỳ sẽ tổng hợp điểm trung bình từ các điểm quá trình, điểm thi giữa kỳ, điểm thi cuối kỳ để đánh giá năng lực học tập của học sinh. 
+    Chức năng xếp thời khoá biểu cho học sinh theo từng lớp """,
 
     'author': "PepeShop",
     'website': "https://edu-pnhhieu-uit.odoo.com/",
